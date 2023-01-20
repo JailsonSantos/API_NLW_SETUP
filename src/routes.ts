7cfcf9df2d4c1b6cfb62f976baadb87415a3c827
JailsonSantos <jailson.ads007@gmail.com>
2023-01-20 16:06:19 -0300

    Feat: Finalizando as Rotas da aplicação

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
+import dayjs from 'dayjs'
 import { prisma } from "./lib/prisma"
 import { FastifyInstance } from "fastify"
-import dayjs from 'dayjs'
 
 export async function appRoutes(app: FastifyInstance) {
 
@@ -75,5 +75,85 @@ export async function appRoutes(app: FastifyInstance) {
     }
 
     // hábitos que já foram completados
+  })
+
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
+    }
+  })
+
+  app.get('/summary', async () => {
+    // Query mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+    // Prima ORM: RAW SQL => Cada código funcina de acordo com as espeficiações de cada banco, 
+    // Nessa caso aqui o SQL => SQLite
+
+    // SubQueries usa-se os () parenteses
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D;
+    `
+
+    return summary
   })
 }
